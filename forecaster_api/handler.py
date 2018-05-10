c028123c59646668bb6f4dc5a763bbbed37b2b5e
David Roberts <droberts@livewatch.com>
2018-05-10 15:50:18 -0500

    pingin like nuts

--- forecaster_api/handler.py
+++ forecaster_api/handler.py
@@ -1,24 +1,11 @@
 import json
 
+def ping(event, context):
+    pong = "pong"
+    return build_response(200, pong)
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
+def build_response(code, body):
+    return {
+        "statusCode": code,
         "body": json.dumps(body)
     }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
