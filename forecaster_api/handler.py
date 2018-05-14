8ffb8ac839d6a61aa2038ed8cbe453e47cb92dac
David Roberts <droberts@livewatch.com>
2018-05-14 15:03:07 -0500

    test wunderground API connection

--- forecaster_api/handler.py
+++ forecaster_api/handler.py
@@ -1,5 +1,7 @@
+import os
 import json
 from dynamo_wrapper import DynamoDB
+import requests
 
 def ping(event, context):
     pong = "pong"
@@ -20,3 +22,24 @@ def build_response(code, body):
         "statusCode": code,
         "body": json.dumps(body)
     }
+
+def test_fetch_forecast(event, context):
+    params = json.loads(event['body'])
+    try:
+        zip_code = params['zip_code']
+    except KeyError:
+        return build_response(400, "error: please supply a zip_code")
+
+    wunderground_url = f'http://api.wunderground.com/api/{wundergound_api_key()}/forecast/q/{zip_code}.json'
+    req = requests.get(wunderground_url)
+    return build_response(200, json.loads(req.text))
+
+def get_forecast(zip_code):
+    wunderground_url = f'http://api.wunderground.com/api/{wundergound_api_key()}/forecast/q/{zip_code}.json'
+    req = requests.get(wunderground_url)
+    return req.json
+
+
+def wundergound_api_key():
+    return os.environ['wunderground_api_key']
+
