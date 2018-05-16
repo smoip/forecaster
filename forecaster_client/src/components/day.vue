74f9f9b84c414b7fca762262e236a03c34db46a3
David Roberts <droberts@livewatch.com>
2018-05-16 17:39:49 -0500

    basic client functionality

---
+++ forecaster_client/src/components/day.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="day">
+      <h3>{{ date.weekday_short }} {{ date.monthname }} {{ date.day }}</h3>
+      <div>
+        <ul class="conditions-group">
+          <li><img class="condition-icon" :src="iconUrl" /></li>
+          <li>{{ conditions }}</li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>High: {{ high.fahrenheit }}{{ this.degreeSymbol() }} F</li>
+          <li>Low: {{ low.fahrenheit }}{{ this.degreeSymbol() }} F</li>
+        </ul>
+      </div>
+      <hr />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Day',
+
+  methods: {
+    degreeSymbol () { return '\u00B0' }
+  },
+
+  props: [
+    'conditions',
+    'date',
+    'high',
+    'low',
+    'iconUrl'
+  ]
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin-top: 0px;
+  margin-bottom: 5px;
+  padding-top: 0px;
+}
+
+.condition-icon {
+  margin-bottom: -10px;
+  width: 40px;
+  height: 40px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+.day {
+  text-align: center;
+  padding-bottom: 20px;
+}
+
+.conditions-group {
+  height: 40px;
+  line-height: 40px;
+  white-space: nowrap;
+}
+hr {
+  width: 12%;
+  margin-top: 30px;
+}
+</style>
--- forecaster_client/src/components/forecaster.vue
+++ forecaster_client/src/components/forecaster.vue
@@ -1,32 +1,93 @@
 <template>
   <div>
-    <h1>Forecaster</h1>
-    <p>
-      Get some forecast data, you dork.
-    </p>
+    <div class="head-material">
+      <h1>Forecaster</h1>
+      <p>
+        Zipcode: <input v-model="zipCode"/>
+      </p>
+      <button @click="getForecast" type="button">Get forecast</button> 
+    </div>
 
-    <button @click="getForecast" type="button"/>
+    <div v-if="showLoader" class="spinner">
+      <img src="../assets/spinner.gif" />
+    </div>
+
+    <div v-if="errorStatus">
+      Error fetching forecast - 
+      Please confirm that {{ zipCode }} is a valid Zipcode
+    </div>
+
+    <div v-if="!showLoader" class="forecast">
+      <Day v-for="day in forecastDays"
+        :key="day.date.weekday_short"
+        :conditions="day.conditions"
+        :date="day.date"
+        :high="day.high"
+        :low="day.low"
+        :iconUrl="day.icon_url" />
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Day from './day.vue'
 
 export default {
   name: 'Forecaster',
 
+  components: { Day },
+
   methods: {
     updateForecast (newForecast) {
-      this.forecast = newForecast
+      if (newForecast.data.forecast === undefined) {
+        this.errorStatus = true
+        return
+      }
+      this.forecast = this.parseForecast(newForecast)
+      this.showLoader = false
+    },
+
+    parseForecast (rawResponse) {
+      let jsonBlob = rawResponse.data.forecast.S
+      return JSON.parse(jsonBlob)
     },
 
     getForecast () {
+      this.showLoader = true
       let data = JSON.stringify({
         zip_code: this.zipCode,
       })
       let headers = { headers: { "x-api-key": this.forecasterApiKey } }
       axios.post(this.forecasterApiUrl, data, headers)
         .then((resp) => this.updateForecast(resp))
+    },
+    
+    // I just don't want to deal with more deps
+    isEmpty (obj) {
+      return Object.keys(obj).length === 0 && obj.constructor === Object
+    },
+
+    limitDays (allForecastDays) {
+      return allForecastDays.slice(0, 3)
+    }
+  },
+
+  created () {
+    this.getForecast
+  },
+
+  computed: {
+    forecastDays () {
+      if (this.forecast.forecast === undefined) {
+        return {}
+      } else {
+        return this.limitDays(this.forecast.forecast.simpleforecast.forecastday)
+      }
+    },
+
+    errorStatus () {
+      return (this.isEmpty(this.forecastDays)) && !this.showLoader
     }
   },
 
@@ -35,27 +96,25 @@ export default {
     return {
       forecasterApiUrl: "https://lio6akrrmb.execute-api.us-east-1.amazonaws.com/dev/fetch_forecast",
       forecasterApiKey: "66nhRGa2hc9Anj0oRAbnJ2nkFKnjeYuv95w27Zho",
-      forecast: {},
-      zipCode: "20230" 
+      forecast: {
+        forecast: {
+          simpleforecast: {
+            forecastday: []
+          }
+        }
+      },
+      zipCode: "20230",
+      showLoader: false
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.head-material {
+  padding-bottom: 40px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.spinner {
+  text-align: center;
 }
 </style>
